8dc48da71db18b9e8ac3dab5bb7b131667c73d38
muyong <muyong_2003@hotmail.com>
2018-05-29 16:18:06 -0400

    Link app.

--- app/src/main/assets/siloam_files/callApp.js
+++ app/src/main/assets/siloam_files/callApp.js
@@ -1,11 +1,11 @@
 $("#enroll").click(function(){
   //alert("will call app");
-  window.location.href = "enroll://enroll";
+  window.location.href = "enroll://enroll?firstName=" + $("#firstname").val() + "&lastName=" + $("#lastname").val();
 });
 
 $("#auth").click(function(){
   //alert("will call app");
-  window.location.href = "auth://auth";
+  window.location.href = "auth://auth?userId=" + $("#userid").val();
 });
 
 
@@ -13,8 +13,22 @@ $(document).ready(function() {
 	ws = new WebSocket("ws://localhost:8025/ws/chat");
 
 	ws.onmessage = function(event) {
+	    alert("get message " + event.data);
 	    var message = JSON.parse(event.data);
-		$("#messages").append("<p>" + message.content + "</p>");
+
+	    if(message.type === "enroll") {
+	        if(message.result === 1) {
+	            alert("success with userid " + message.userid);
+	            $("#messages").append("<p>User enroll success!</p>");
+	            $("#messages").append("<p>User id: " + message.userid + "</p>");
+	        } else
+	            $("#messages").append("<p>User enroll not success.</p>");
+	    } else if(message.type === "auth") {
+	        if(message.result === 1)
+                $("#messages").append("<p>User verified!</p>");
+            else
+                $("#messages").append("<p>User verification fails.</p>");
+	    }
 	};
 
 	ws.onclose = function() {
